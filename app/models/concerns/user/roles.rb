79ab1cad8b26673832bb7dc96132d5395c743017
Mario Andrés Correa <a@macool.me>
2015-02-01 19:29:08 -0500

    add `User::Roles`

---
+++ app/models/concerns/user/roles.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  module Roles
+    extend ActiveSupport::Concern
+
+    ROLES = %w(admin moderador curador cliente)
+
+    included do
+      validates :role, inclusion: {in: ROLES}
+    end
+
+    # allows calling of `#admin?` `#moderador?`
+    ROLES.each do |role|
+      define_method "#{role}?" do
+        role == role
+      end
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Roles
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
   devise :database_authenticatable, :recoverable,
