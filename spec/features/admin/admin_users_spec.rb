41c4833e2f13bd97ecf190021c3fe2aa1621aa1c
Mario Andrés Correa A <admin@macool.me>
2015-02-02 19:33:26 +0000

    spec for users#show @leninkp: así de fáciles

--- spec/features/admin/admin_users_spec.rb
+++ spec/features/admin/admin_users_spec.rb
@@ -19,6 +19,16 @@ describe "user management" do
       expect(page).to have_text(other_user.email)
     }
   end
+  
+  feature "see a user" do
+    before {
+      visit admin_user_path(current_user)
+    }
+    
+    it {
+      expect(page).to have_text(current_user.name)
+    }
+  end
 
   context "with forms" do
     let(:user_attrs) { attributes_for :user }
