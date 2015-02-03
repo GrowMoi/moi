5a477a11429cf78d9f5eaac088f88424f212666e
Fabricio Flores <fabrifloresg@gmail.com>
2015-02-03 14:24:40 -0500

    add capybara-webkit and add methods to work js [ci skip]

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :test, :development do
   gem 'factory_girl_rails'
   gem 'capybara'
   gem 'pry'
+  gem 'capybara-webkit'
+  gem 'database_cleaner'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require 'capybara/rails'
+require 'capybara/webkit/matchers'
+Capybara.javascript_driver = :webkit
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -27,10 +29,20 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.before(:each) do
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.include(Capybara::Webkit::RspecMatchers, :type => :feature)
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -45,7 +57,7 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
+  # config.infer_spec_type_from_file_location!
 
   # include factorygirl methods
   config.include FactoryGirl::Syntax::Methods
