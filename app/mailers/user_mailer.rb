26cd81b756f52e030de011f5ff049860911f824a
Lenin Capa <leviskp87@gmail.com>
2015-02-09 22:51:02 -0500

    change names

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
 	default from: 'moi@example.com'
 
-  def change_permission(user)
+  def notify_role_change(user)
     @user = user
-    mail(to: @user.email, subject: I18n.t("email.subject"))
+    mail(to: @user.email, subject: I18n.t("user_mailer.change_permission.subject"))
   end
 end
