4b8909e42168055de545d5913f256c3fbf41f1d7
Mario Andrés Correa <a@macool.me>
2015-02-10 00:42:41 -0500

    set name for admin on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ unless User.exists?(role: "admin")
   # create admin if there's no admin
   email = "admin@example.com"
   password = "12345678"
-  User.create!(email: email, password: password, role: "admin")
+  User.create!(name: "admin", email: email, password: password, role: "admin")
   puts "Se ha creado el administrador #{email} con contraseña #{password}"
 end
