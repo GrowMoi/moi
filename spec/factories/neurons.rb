f89f79ba5d0f6cc9b31c0f7bb70df1f42da8c4bf
Fabricio Flores <fabrifloresg@gmail.com>
2015-02-19 09:32:48 -0500

    little changes - annotate - add paper_trail tracking to neuron model

--- spec/factories/neurons.rb
+++ spec/factories/neurons.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: neurons
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  parent_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :neuron do
     sequence(:title) { |n| "Neurona #{n}" }
