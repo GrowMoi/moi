92aeed416e87a1eee75da55c9ec00fecdaa180b7
Fabricio Flores <fabrifloresg@gmail.com>
2015-02-19 09:32:48 -0500

    little changes - annotate - add paper_trail tracking to neuron model

--- app/models/neuron.rb
+++ app/models/neuron.rb
@@ -1,7 +1,18 @@
+# == Schema Information
+#
+# Table name: neurons
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  parent_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Neuron < ActiveRecord::Base
   belongs_to :parent, class: Neuron
   #paper trail gem
-  has_paper_trail
+  has_paper_trail only: [:title, :parent_id]
 
   begin :validations
     validates :title, presence: true,
