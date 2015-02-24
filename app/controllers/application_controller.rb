4930e8db99916299e754e13a6426c55c30dfe171
Fabricio Flores <fabrifloresg@gmail.com>
2015-02-23 22:21:13 -0500

    remove code from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :error
 
-  rescue_from Exception, with: :render_resource_error
-
   # strong_params in decent exposure
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
@@ -15,14 +13,6 @@ class ApplicationController < ActionController::Base
   # cancan
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, error: I18n.t("views.unauthorized")
-  end
-
-  def render_resource_error(exception)
-    if Rails.env == "production" || Rails.env == "staging"
-      Airbrake.notify(exception, airbrake_request_data)
-      flash[:error] = exception.message
-      redirect_to root_path
-    end
   end
 
 end
