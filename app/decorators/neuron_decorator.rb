7b9ae27f564702eeba6081c893a94cd55b95516f
Lenin Capa <leviskp87@gmail.com>
2015-03-14 19:35:00 -0500

    change show neuron

--- app/decorators/neuron_decorator.rb
+++ app/decorators/neuron_decorator.rb
@@ -2,4 +2,10 @@ class NeuronDecorator < LittleDecorator
   def decorated_contents
     @decorated_contents ||= decorate(contents)
   end
+
+  def parent_title
+    if parent_id
+      title = Neuron.find(parent_id).title
+    end
+  end
 end
