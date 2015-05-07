81dd90c65900182d3922d7da27b9cd6cb4670eca
Fabricio Flores <fabrifloresg@gmail.com>
2015-05-07 10:16:13 -0500

    Fixes bug that returns to app after logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_cache_buster
 
   add_flash_types :error
 
@@ -20,4 +21,11 @@ class ApplicationController < ActionController::Base
   expose(:decorated_current_user) {
     decorate current_user
   }
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
 end
