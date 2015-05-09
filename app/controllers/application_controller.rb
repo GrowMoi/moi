c9aae8a2d63bf8c53867a6d00bc083238046e93a
Fabricio Flores <fabrifloresg@gmail.com>
2015-05-08 22:35:04 -0500

    Remove headers cache only on logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_cache_buster
 
   add_flash_types :error
 
@@ -22,10 +21,17 @@ class ApplicationController < ActionController::Base
     decorate current_user
   }
 
+  private
+
   def set_cache_buster
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    set_cache_buster
+    root_path
   end
 
 end
