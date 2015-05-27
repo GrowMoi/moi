ff4f4580e28dd766a5d4f7fccc15577e38ddf5d4
Mario Andrés Correa <a@macool.me>
2015-05-27 10:26:10 -0500

    minor landing page enhances

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,27 @@
 module Admin
   class DashboardController < AdminController::Base
     expose(:neurons) {
-      Neuron.try(neurons_state.to_sym)
+      Neuron.where(state: state)
     }
     expose(:neurons_state) {
-      params[:state] ||= "approved"
+      state = params[:state]
+      if Neuron.states.has_key?(state)
+        state
+      else
+        default_state
+      end
     }
+
+    private
+
+    def state
+      Neuron.states.fetch(params[:state]) {
+        Neuron.states.fetch(default_state)
+      }
+    end
+
+    def default_state
+      "approved"
+    end
   end
 end
--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -7,16 +7,23 @@ RSpec.describe Admin::DashboardController,
       create :user, :admin
     }
 
-    #default neuron is state pending
+    # default neuron state is pending
     let!(:pending_neuron) { create :neuron }
 
     let!(:approved_neuron) {
       create :neuron, state: :approved
     }
 
+    before {
+      sign_in current_user
+    }
+
+    it {
+      expect(pending_neuron.state).to eq("pending")
+    }
+
     describe "should get approved neuron" do
       before {
-        sign_in current_user
         get :index, state: 'approved'
       }
 
@@ -27,13 +34,22 @@ RSpec.describe Admin::DashboardController,
 
     describe "should get pending neuron" do
       before {
-        sign_in current_user
         get :index, state: 'pending'
       }
 
       it {
         expect(controller.neurons).to include(pending_neuron)
       }
+    end
+
+    describe "invalid params" do
+      before {
+        get :index, state: "something-bad"
+      }
+
+      it "uses default scope" do
+        expect(controller.neurons).to include(approved_neuron)
+      end
     end
   end
 end
