6f474a0830fa74bc29ff85290d57115ce94be869
Lenin Capa <leviskp87@gmail.com>
2015-05-28 18:40:08 -0500

    add spellchecker in content decorator

--- app/decorators/content_decorator.rb
+++ app/decorators/content_decorator.rb
@@ -14,4 +14,29 @@ class ContentDecorator < LittleDecorator
               target: "_blank"
     end
   end
+
+  def spellcheck_description
+    description = record.description || ""
+    results = Spellchecker.check(description, dictionary = I18n.locale.to_s)
+    words = content_tag :div do
+      content_words = content_tag(:span)
+      results.each do |w|
+        if w[:correct]
+          word = w[:original]
+        else
+          suggestions = w[:suggestions].take(3)
+          suggestions = suggestions.join(" | ")
+          word = content_tag(:span, "#{w[:original]}",
+            class: " bs-tooltip text-danger",
+            title: suggestions,
+            "data-toggle" => "tooltip",
+            "data-placement" => "top" )
+        end
+        content_words = content_words + " " + word
+      end
+      content_words
+    end
+    return words
+  end
+
 end
