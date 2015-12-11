56bec7c2d79595f315448a6f0a2ba0a1b15a6bc3
Mario Andrés Correa <a@macool.me>
2015-12-10 23:33:27 -0500

    add `SpellcheckDecorator`
    to leverage `ContentDecorator`

--- app/decorators/content_decorator.rb
+++ app/decorators/content_decorator.rb
@@ -53,26 +53,8 @@ class ContentDecorator < LittleDecorator
            }
   end
 
-  def description_spellchecked
-    if spellchecked["description"].present?
-      spellchecked["description"].html_safe
-    elsif record.description.present?
-      content_tag(
-        :span,
-        record.description
-      ) + spellcheck_error.html_safe
-    end
-  end
-
-  def title_spellchecked
-    if spellchecked["title"].present?
-      spellchecked["title"].html_safe
-    elsif record.title.present?
-      content_tag(
-        :span,
-        record.title
-      ) + spellcheck_error.html_safe
-    end
+  def spellchecked(name)
+    spellcheck_analysis.spellchecked(name.to_s)
   end
 
   def approved_to_s
@@ -118,39 +100,7 @@ class ContentDecorator < LittleDecorator
     end
   end
 
-  def spellcheck_error
-    content_tag(
-      :span,
-      nil,
-      class: "glyphicon glyphicon-exclamation-sign bs-tooltip spellcheck-error",
-      title: I18n.t("views.contents.aspell_not_present")
-    )
-  end
-
-  def spellchecked
-    @spellchecked ||= spellcheck_analyses.inject({}) do |memo, analysis|
-      if analysis.success?
-        memo[analysis.attr_name] = spellchecked_attr(analysis)
-      end
-      memo
-    end
-  end
-
-  def spellchecked_attr(analysis)
-    analysis.words.inject(
-      record.send(analysis.attr_name)
-    ) do |text, word|
-      text.gsub(
-        word["original"],
-        suggestions_for(word)
-      )
-    end
-  end
-
-  def suggestions_for(word)
-    content_tag(:span,
-                word["original"],
-                class: "bs-tooltip text-danger",
-                title: word["suggestions"].join(" | "))
+  def spellcheck_analysis
+    @spellcheck_analysis ||= SpellcheckDecorator.new(record, self)
   end
 end
