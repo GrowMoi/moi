4e14e9e26b6f966ea5c245851171ad6c351e179b
Mario Andrés Correa <a@macool.me>
2016-03-02 15:01:28 -0500

    use cloudinary cdn for staging

--- app/uploaders/content_media_uploader.rb
+++ app/uploaders/content_media_uploader.rb
@@ -1,8 +1,11 @@
 # encoding: utf-8
 
 class ContentMediaUploader < CarrierWave::Uploader::Base
-  storage :file
-  # storage :fog
+  if defined?(Cloudinary)
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
