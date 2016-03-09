fd3359a9bd86cdeeb70ba9940f666f90a5a51ecd
Mario Andrés Correa <a@macool.me>
2016-03-08 20:37:30 -0500

    refactor log classes

--- app/decorators/paper_trail/neuron_version_decorator.rb
+++ app/decorators/paper_trail/neuron_version_decorator.rb
@@ -1,5 +1,7 @@
 module PaperTrail
   class NeuronVersionDecorator < VersionDecorator
+    include Relationships
+
     def changed_attrs
       changed_keys.map { |key|
         t("activerecord.attributes.neuron.#{key}").downcase
@@ -10,55 +12,6 @@ module PaperTrail
       t(
         "views.changelog.time_ago",
         time_ago: time_ago_in_words(created_at)
-      )
-    end
-
-    def contents_changed?
-      raw_contents.length > 0
-      # may have to enhance:
-      # Version.exists?(
-      #   item_type: "Content",
-      #   transaction_id: transaction_id
-      # )
-    end
-
-    def medium_changed?
-      raw_medium.length > 0
-    end
-
-    def links_changed?
-      raw_links.length > 0
-    end
-
-    def videos_changed?
-      raw_videos.length > 0
-    end
-
-    def changed_videos
-      @changed_videos ||= decorate(
-        raw_videos,
-        ContentVideoVersionDecorator
-      )
-    end
-
-    def changed_links
-      @changed_links ||= decorate(
-        raw_links,
-        ContentLinkVersionDecorator
-      )
-    end
-
-    def changed_contents
-      @changed_contents ||= decorate(
-        raw_contents,
-        ContentVersionDecorator
-      )
-    end
-
-    def changed_medium
-      @changed_medium ||= decorate(
-        raw_medium,
-        ContentMediaVersionDecorator
       )
     end
 
@@ -92,40 +45,9 @@ module PaperTrail
     end
 
     def extra_keys
-      Array.new.tap do |keys|
-        keys << "contents" if contents_changed?
-        keys << "medium" if medium_changed?
-        keys << "links" if links_changed?
-        keys << "videos" if videos_changed?
+      RELATIONSHIPS.select do |relationship|
+        send "#{relationship}_changed?"
       end
-    end
-
-    def raw_contents
-      @raw_contents ||= Version.where(
-        item_type: "Content",
-        transaction_id: transaction_id
-      )
-    end
-
-    def raw_medium
-      @raw_medium ||= Version.where(
-        item_type: "ContentMedia",
-        transaction_id: transaction_id
-      )
-    end
-
-    def raw_links
-      @raw_links ||= Version.where(
-        item_type: "ContentLink",
-        transaction_id: transaction_id
-      )
-    end
-
-    def raw_videos
-      @raw_videos ||= Version.where(
-        item_type: "ContentVideo",
-        transaction_id: transaction_id
-      )
     end
   end
 end
