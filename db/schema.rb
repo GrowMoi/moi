0c2223d412688f962627ee4be0b8104a22b99361
Mario Andrés Correa <a@macool.me>
2016-03-09 15:00:57 -0500

    wipe remaining old videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308034712) do
+ActiveRecord::Schema.define(version: 20160309195300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
