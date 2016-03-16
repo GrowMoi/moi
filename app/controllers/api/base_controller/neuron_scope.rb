e359c7b3c5796111d1105eb147eff5defad32409
Mario Andrés Correa <a@macool.me>
2016-03-15 19:45:49 -0500

    wipe todo comments

--- app/controllers/api/base_controller/neuron_scope.rb
+++ app/controllers/api/base_controller/neuron_scope.rb
@@ -8,19 +8,8 @@ module Api
           TreeService::PublicScopeFetcher.new(current_user)
         }
         expose(:neurons) {
-          # TODO: revise scope (see #neurons_to_learn)
-          neuron_scope.neurons # .accessible_by(current_ability) -> shouldn't be quering only public ?
-                      .includes(:contents)
-        }
-        expose(:neurons_to_learn) {
-          # TODO: this should be `neurons` scope
-          # scope should be the neurons the user
-          # has learnt + the neurons he's yet to
-          # learn
-          # ATM only root is sent to the user,
-          # until we figure out how to learn, etc
-          # [ root_neuron ]
           neuron_scope.neurons
+                      .includes(:contents)
         }
         expose(:neuron)
       end
