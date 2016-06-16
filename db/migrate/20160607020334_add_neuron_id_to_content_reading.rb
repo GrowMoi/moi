06a8668ad518ce15aada017e3f9cfa4acde8d254
Mario Andrés <a@macool.me>
2016-06-16 09:53:24 -0500

    wipe all readings

--- db/migrate/20160607020334_add_neuron_id_to_content_reading.rb
+++ db/migrate/20160607020334_add_neuron_id_to_content_reading.rb
@@ -1,5 +1,8 @@
 class AddNeuronIdToContentReading < ActiveRecord::Migration
   def change
+    say_with_time "WARNING: wiping all readings" do
+      ContentReading.destroy_all
+    end
     add_reference :content_readings,
                   :neuron,
                   null: false,
