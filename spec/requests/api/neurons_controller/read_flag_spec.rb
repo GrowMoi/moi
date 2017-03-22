6391ab3d3257be59e696e9e2f3ff7eadff775e7b
Jhymer Martínez <jmartinez@ioet.com>
2017-03-22 17:22:30 -0500

    Add pending specs

--- spec/requests/api/neurons_controller/read_flag_spec.rb
+++ spec/requests/api/neurons_controller/read_flag_spec.rb
@@ -38,4 +38,22 @@ RSpec.describe Api::NeuronsController,
       ).to be_truthy
     }
   end
+
+  describe "content can be readed" do
+    let!(:reading) {
+      create :content_reading,
+             content: content,
+             user: current_user
+    }
+
+    before {
+      get "/api/neurons/#{neuron.id}"
+    }
+
+    it {
+      expect(
+        subject["can_read"]
+      ).to be_truthy
+    }
+  end
 end
