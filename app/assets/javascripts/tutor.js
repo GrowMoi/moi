e39570e9dbb8164506e9f39c3d76e5856978aff5
Jhymer Martínez <jmartinez@ioet.com>
2017-04-28 02:54:25 -0500

    Add Tutor view

--- app/assets/javascripts/tutor.js
+++ app/assets/javascripts/tutor.js
@@ -2,16 +2,5 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require bootstrap
-//= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require turbolinks
-//= require d3
-//= require nprogress
-//= require jquery_nested_form
-//= require chosen.jquery.min
-//= require bootstrap-tagsinput.min
-//= require handlebars
-//= require jquery-sortable-min.js
 //= require_tree ./tutor
