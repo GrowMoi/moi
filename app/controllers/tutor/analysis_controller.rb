d3d66885dd31006fcbc8d5539e1256bd92628eb1
Jhymer Martínez <jmartinez@ioet.com>
2017-05-06 12:44:43 -0500

    Add used time

--- app/controllers/tutor/analysis_controller.rb
+++ app/controllers/tutor/analysis_controller.rb
@@ -1,5 +1,6 @@
 module Tutor
   class AnalysisController < TutorController::Base
+    require 'time_diff'
 
     expose(:client_data) {
       User.where(id: params[:id], role: :cliente)
@@ -17,6 +18,7 @@ module Tutor
       @client = client_data.first
       @statistics = @client.generate_statistics
       @statistics["total_right_questions"] = total_right_questions(@statistics["user_tests"])
+      @statistics["used_time"] = time_diff(@statistics)
     end
 
     def total_right_questions(tests)
@@ -29,6 +31,13 @@ module Tutor
         end
       end
       count
+    end
+
+    def time_diff(statistics)
+      start_d = statistics["user_created_at"]
+      end_d = statistics["user_updated_at"]
+      diff_time = Time.diff(start_d, end_d)
+      diff_time[:diff]
     end
   end
 end
