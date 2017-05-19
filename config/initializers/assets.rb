ce9881b093746c5621db11fac2fc67a729541635
Jhymer Martínez <jmartinez@ioet.com>
2017-05-19 12:02:23 -0500

    Improvements for Tutor View (#176)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,9 +12,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
+  *.woff
   admin.js
   admin.css
   tutor.js
   tutor.css
-  *.woff
 )
