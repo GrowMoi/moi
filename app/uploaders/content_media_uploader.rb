d2aa87b567f824d4b9c9ddd9bfb07596a1538a29
Lenin Capa <leviskp87@gmail.com>
2017-06-14 14:42:11 -0500

    dinamic name file

--- app/uploaders/content_media_uploader.rb
+++ app/uploaders/content_media_uploader.rb
@@ -12,4 +12,16 @@ class ContentMediaUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  ##
+  # use a timestamp in filenames
+  # https://github.com/carrierwaveuploader/carrierwave/wiki/How-to%3A-Use-a-timestamp-in-file-names
+  def filename
+    @name ||= "#{timestamp}-#{super}" if original_filename.present? and super.present?
+  end
+
+  def timestamp
+    var = :"@#{mounted_as}_timestamp"
+    model.instance_variable_get(var) or model.instance_variable_set(var, Time.now.to_i)
+  end
 end
