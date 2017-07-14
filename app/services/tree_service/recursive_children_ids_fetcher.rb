cfcd950d43a3da199e3fa657338fbf3e1fd6b534
Jhymer Martínez <jmartinez@ioet.com>
2017-07-13 21:39:20 -0500

    Show content readings by branch

---
+++ app/services/tree_service/recursive_children_ids_fetcher.rb
@@ -0,0 +1,27 @@
+module TreeService
+  class RecursiveChildrenIdsFetcher
+    def initialize(neuron)
+     @neuron = neuron
+      @ids = []
+    end
+
+    def children_ids
+      while ids_for_level = fetch_ids_for_level
+        @ids += ids_for_level
+      end
+      @ids
+    end
+
+    private
+
+    def fetch_ids_for_level
+      @level_ids ||= @neuron.id
+      scope = Neuron.where(parent_id: @level_ids)
+      @level_ids = scope.pluck(:id)
+      if scope.count > 0
+        return @level_ids
+      end
+      return nil
+    end
+  end
+end
