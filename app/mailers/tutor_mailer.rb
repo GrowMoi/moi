b8d0cbb4b74d9e05bdca9faa9f77eb9075e430ef
Jhymer Martínez <jmartinez@ioet.com>
2017-12-11 23:25:14 -0500

    Improvements

--- app/mailers/tutor_mailer.rb
+++ app/mailers/tutor_mailer.rb
@@ -2,9 +2,10 @@ class TutorMailer < ApplicationMailer
   default from: 'Moi Aprendizaje Social <noreply@growmoi.com>'
 
   def achievement_notification(tutor, client, achievement)
-    @tutor = tutor
-    @client = client
-    @achievement = achievement
-    mail(to: @tutor.email, subject: I18n.t("tutor_mailer.achievement_notification.subject"))
+    @tutor_name = if tutor.name.present? then tutor.name else tutor.username end
+    @client_name = if client.name.present? then client.name else client.username end
+    @achievement_name = achievement.name
+    @url  = "http://www.growmoi.com"
+    mail(to: tutor.email, subject: I18n.t("tutor_mailer.achievement_notification.subject"))
   end
 end
