3262fe8ab97b0afb5e9f58bbb236a9d5955d24c9
Lenin Capa <leviskp87@gmail.com>
2018-02-21 10:34:03 -0500

    add successful_tests and content_summary fields into profile API

--- app/controllers/api/users/profiles_controller.rb
+++ app/controllers/api/users/profiles_controller.rb
@@ -10,7 +10,28 @@ module Api
       api :GET,
           "/users/profile",
           "Get basic information user"
-
+      example %q{
+        "id":6082,
+        "email":"test1987@gmail.com",
+        "name":null,
+        "username":"test1987",
+        "country":"Ecuador",
+        "school":"Test School",
+        "city":"Loja",
+        "last_contents_learnt":[
+          {
+            "id":1246,
+            "media":["http://localhost:5000/uploads/content_media/media/160/photosynthesis-powerpoint-8-728.jpg"],
+            "title":"¿Cómo transforma energía la Naturaleza?",
+            "neuron_id":2350
+          }
+        ]
+        "test_without_errors":1,
+        "content_summary":{
+          "current_learnt_contents":10,
+          "total_approved_contents":314
+        }
+      }
       def show
         respond_with(
           user,
--- app/serializers/api/user_profile_serializer.rb
+++ app/serializers/api/user_profile_serializer.rb
@@ -35,19 +35,12 @@ module Api
                :username,
                :country,
                :school,
-               :birthday,
                :age,
                :city,
                :last_contents_learnt,
-               :tree_image
-
-    def age
-      birthday = object.birthday
-      if birthday
-        now = Time.now.utc.to_date
-        now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
-      end
-    end
+               :tree_image,
+               :successful_tests,
+               :content_summary
 
     def last_contents_learnt
       object.content_learnings.last(4).map do |content_learnt|
@@ -63,6 +56,17 @@ module Api
 
     def tree_image
       object.tree_image.url
+    end
+
+    def successful_tests
+      object.total_successful_tests
+    end
+
+    def content_summary
+      {
+        current_learnt_contents: object.content_learnings.count,
+        total_approved_contents: Content.approved.count
+      }
     end
   end
 end
