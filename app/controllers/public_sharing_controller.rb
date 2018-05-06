a956bb2895f9061b24aff26cebfe1cb3cad82d0b
Mario Andrés Correa <a@macool.me>
2018-05-06 11:10:07 -0600

    WIP

--- app/controllers/public_sharing_controller.rb
+++ app/controllers/public_sharing_controller.rb
@@ -2,12 +2,17 @@ class PublicSharingController < ApplicationController
   layout false
 
   def show
-    puts "UA!"
-    puts request.user_agent
     @social_sharing = SocialSharing.friendly.find(params[:id])
+    unless agent_is_facebook?
+      redirect_to(@social_sharing.uri)
+    end
   end
 
   private
+
+  def agent_is_facebook?
+    request.user_agent =~ Regexp.new("facebookexternalhit/1.1")
+  end
 
   def facebook_app_id
     Rails.application.secrets.facebook_app_id
