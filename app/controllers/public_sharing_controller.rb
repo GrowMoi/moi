004043c19c1118d15549a81d38f9c8c0e661f3bf
Mario Andrés Correa <a@macool.me>
2018-05-06 11:04:18 -0600

    WIP

--- app/controllers/public_sharing_controller.rb
+++ app/controllers/public_sharing_controller.rb
@@ -2,6 +2,15 @@ class PublicSharingController < ApplicationController
   layout false
 
   def show
+    puts "UA!"
+    puts request.user_agent
     @social_sharing = SocialSharing.friendly.find(params[:id])
   end
+
+  private
+
+  def facebook_app_id
+    Rails.application.secrets.facebook_app_id
+  end
+  helper_method :facebook_app_id
 end
