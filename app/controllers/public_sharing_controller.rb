a80ca00ce68c19090d703a1890da2d33744a3282
Mario Andrés Correa <a@macool.me>
2018-05-25 23:01:33 -0600

    opengraph markup for twitter

--- app/controllers/public_sharing_controller.rb
+++ app/controllers/public_sharing_controller.rb
@@ -3,7 +3,7 @@ class PublicSharingController < ApplicationController
 
   def show
     @social_sharing = SocialSharing.friendly.find(params[:id])
-    if !agent_is_facebook? && !Rails.env.test?
+    if !agent_is_facebook? && !agent_is_twitter? && !Rails.env.test?
       redirect_to(@social_sharing.uri)
     end
   end
@@ -11,11 +11,20 @@ class PublicSharingController < ApplicationController
   private
 
   def agent_is_facebook?
-    request.user_agent =~ Regexp.new("facebookexternalhit/1.1")
+    request.user_agent =~ Regexp.new("facebookexternalhit", Regexp::IGNORECASE)
+  end
+
+  def agent_is_twitter?
+    request.user_agent =~ Regexp.new("twitterbot", Regexp::IGNORECASE)
   end
 
   def facebook_app_id
     Rails.application.secrets.facebook_app_id
   end
   helper_method :facebook_app_id
+
+  def twitter_site
+    Rails.application.secrets.twitter_site
+  end
+  helper_method :twitter_site
 end
