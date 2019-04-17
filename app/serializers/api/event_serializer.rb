da3686425ec1dd59676d4879d74843e0f88749c1
Lenin Capa <leviskp87@gmail.com>
2019-04-17 14:41:28 -0500

    return color neurons v1 W.I.P

--- app/serializers/api/event_serializer.rb
+++ app/serializers/api/event_serializer.rb
@@ -34,11 +34,29 @@ module Api
 
     def contents
       ids = object.content_ids.map.reject { |id| id.empty? }
+      branches_neurons_ids = get_neurons_id_by_branch
       ids.map do |id|
         content = Content.find(id)
+        neuron = content.neuron
+        root_neuron = TreeService::RootFetcher.root_neuron
+        color = nil
+        # binding.pry
+        unless (branches_neurons_ids[:language][:ids] & [neuron.id]).empty?
+          color = branches_neurons_ids[:language][:color]
+        end
+        unless (branches_neurons_ids[:art][:ids] & [neuron.id]).empty?
+          color = branches_neurons_ids[:art][:color]
+        end
+        unless (branches_neurons_ids[:learn][:ids] & [neuron.id]).empty?
+          color = branches_neurons_ids[:learn][:color]
+        end
+        unless (branches_neurons_ids[:nature][:ids] & [neuron.id]).empty?
+          color = branches_neurons_ids[:nature][:color]
+        end
         {
           content_id: id,
-          neuron: content.neuron.title
+          neuron: neuron.title,
+          color: root_neuron.id == neuron.id ? 'yellow' : color
         }
       end
     end
@@ -51,6 +69,42 @@ module Api
       event_user = current_user.user_events.find_by_event_id(object.id)
       event_user ? event_user.completed : false
     end
+
+    def get_neurons_id_by_branch
+      branches = {
+        language: {
+          name: 'Lenguaje',
+          color: 'yellow',
+          ids: []
+        },
+        art: {
+          name: 'Artes',
+          color: 'red',
+          ids: []
+        },
+        learn: {
+          name: 'Aprender',
+          color: 'blue',
+          ids: []
+        },
+        nature: {
+          name: 'Naturaleza',
+          color: 'green',
+          ids: []
+        }
+      }
+      branches.each do |key, value|
+        branch = value[:name].downcase
+        neuron_branch = Neuron.where('lower(title) = ?', branch).first
+        branch_ids = TreeService::RecursiveChildrenIdsFetcher.new(
+          neuron_branch
+        ).children_ids
+        branch_ids = branch_ids << neuron_branch.id
+        value[:ids] = branch_ids
+      end
+      branches
+    end
+
 
     alias_method :current_user, :scope
   end
