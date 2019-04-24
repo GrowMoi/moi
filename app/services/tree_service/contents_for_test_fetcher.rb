9420aa007afbee4388a734ba80e9ac7e50020673
Lenin Capa <leviskp87@gmail.com>
2019-04-24 17:56:23 -0500

    fix test with events

--- app/services/tree_service/contents_for_test_fetcher.rb
+++ app/services/tree_service/contents_for_test_fetcher.rb
@@ -7,9 +7,10 @@ module TreeService
     end
 
     def contents
-      readings = readings.map(&:content_id)
-      learnings = learnings.map(&:content_id)
-      readings - learnings
+      readings = @user.content_readings.map(&:content_id)
+      learnings = @user.content_learnings.map(&:content_id)
+      content_to_learn = readings - learnings
+      Content.where( id: content_to_learn )
       # content_ids = [
       #   grouped_content_reading_ids_for_test,
       #   flourished_neurons_content_reading_ids_for_test
@@ -21,52 +22,52 @@ module TreeService
       # )
     end
 
-    private
-
-    def grouped_content_reading_ids_for_test
-      readings.where.not(
-        id: learnings.pluck(:content_id)
-      ).group(:neuron_id).count.map do |neuron_id, count|
-        if count >= MIN_CONTENTS_PER_NEURON
-          readings.where(
-            neuron_id: neuron_id
-          ).pluck(:content_id)
-        end
-      end.flatten.compact
-    end
-
-    def flourished_neurons_content_reading_ids_for_test
-      readings.where(
-        content_id: flourished_neurons_children_content_ids
-      ).pluck(:content_id)
-    end
-
-    def flourished_neurons_children_content_ids
-      neuron_ids = [
-        flourished_neurons_ids,
-        flourished_neurons_children_ids
-      ].flatten.compact
-      Content.where(
-        neuron_id: neuron_ids
-      ).pluck(:id)
-    end
-
-    def flourished_neurons_children_ids
-      Neuron.where(
-        parent_id: flourished_neurons_ids
-      ).pluck(:id)
-    end
-
-    def flourished_neurons_ids
-      learnings.pluck(:neuron_id)
-    end
-
-    def learnings
-      @user.content_learnings
-    end
-
-    def readings
-      @user.content_readings
-    end
+    # private
+    #
+    # def grouped_content_reading_ids_for_test
+    #   readings.where.not(
+    #     id: learnings.pluck(:content_id)
+    #   ).group(:neuron_id).count.map do |neuron_id, count|
+    #     if count >= MIN_CONTENTS_PER_NEURON
+    #       readings.where(
+    #         neuron_id: neuron_id
+    #       ).pluck(:content_id)
+    #     end
+    #   end.flatten.compact
+    # end
+    #
+    # def flourished_neurons_content_reading_ids_for_test
+    #   readings.where(
+    #     content_id: flourished_neurons_children_content_ids
+    #   ).pluck(:content_id)
+    # end
+    #
+    # def flourished_neurons_children_content_ids
+    #   neuron_ids = [
+    #     flourished_neurons_ids,
+    #     flourished_neurons_children_ids
+    #   ].flatten.compact
+    #   Content.where(
+    #     neuron_id: neuron_ids
+    #   ).pluck(:id)
+    # end
+    #
+    # def flourished_neurons_children_ids
+    #   Neuron.where(
+    #     parent_id: flourished_neurons_ids
+    #   ).pluck(:id)
+    # end
+    #
+    # def flourished_neurons_ids
+    #   learnings.pluck(:neuron_id)
+    # end
+    #
+    # def learnings
+    #   @user.content_learnings || []
+    # end
+    #
+    # def readings
+    #   @user.content_readings || []
+    # end
   end
 end
