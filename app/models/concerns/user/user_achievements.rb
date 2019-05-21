1001a7c0ff1b9b97b144d37d2dedb36da655113e
Lenin Capa <leviskp87@gmail.com>
2019-05-21 17:27:17 -0500

    updte status super event

--- app/models/concerns/user/user_achievements.rb
+++ app/models/concerns/user/user_achievements.rb
@@ -10,6 +10,11 @@ class User < ActiveRecord::Base
         if achievement.user_win_achievement?(self)
           new_achievements << UserAdminAchievement.create!(user_id: self.id, admin_achievement_id: achievement.id)
           notify_tutors(self, new_achievements)
+          if completed_super_event
+            super_event = self.user_event_achievements.last
+            super_event.status = "completed"
+            super_event.save
+          end
         end
       end
       new_achievements
@@ -59,6 +64,26 @@ class User < ActiveRecord::Base
     def get_tutor_ids(client)
       UserTutor.where(user: client, status: :accepted)
                .pluck(:tutor_id)
+    end
+
+    def completed_super_event
+      completed = false
+      if super_event_is_valid_yet?
+        ids_super_event = self.user_event_achievements.last.event_achievement.user_achievement_ids
+        ids_user_achievements = self.user_admin_achievements.map(&:id)
+        result = ids_achievements - ids_super_event #give a [] is not pending achievements
+        completed = result.empty?
+      end
+    end
+
+    def super_event_is_valid_yet?
+      valid = false
+      user_take_event = self.user_event_achievements.last
+      if user_take_event
+        super_event = user_take_event.event_achievement
+        is_in_range = super_event.start_date < Time.now && super_event.end_date > Time.now
+        valid = is_in_range && super_event.status == "taken"
+      end
     end
   end
 end
