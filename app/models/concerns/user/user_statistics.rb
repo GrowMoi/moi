d1eca050a448b773b37302752f0c16cb6d86e396
Jhymer Martínez <jhmartinez1991@gmail.com>
2019-09-10 10:48:00 -0500

    Add Improvements

--- app/models/concerns/user/user_statistics.rb
+++ app/models/concerns/user/user_statistics.rb
@@ -202,8 +202,8 @@ class User < ActiveRecord::Base
             }
           }
           if test[:answers].present?
-            answer = test[:answers].detect {|a| a["content_id"] == question["content_id"]}
-            data[:question][:correct_answer] = answer && answer["correct"] ? answer["correct"] : false
+            answer = test[:answers].detect {|a| a["content_id"] == question["content_id"]} || {}
+            data[:question][:correct_answer] = !!answer["correct"]
           end
           learning_tests.push data
         end
