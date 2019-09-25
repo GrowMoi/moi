05644dc5279083a3bc306693c250f113510117ae
Jhymer Martínez <jhmartinez1991@gmail.com>
2019-09-25 11:21:44 -0500

    Change i18n import position

--- app/assets/javascripts/tutor.js
+++ app/assets/javascripts/tutor.js
@@ -6,11 +6,11 @@
 //= require jquery-ui/widgets/selectable
 //= require bootstrap
 //= require pusher
+//= require i18n.js
+//= require i18n/translations
 //= require read-excel-file.min
 //= require chosen.jquery.min
 //= require jquery.infinite-pages
-//= require i18n.js
-//= require i18n/translations
 //= require webcomponentsjs/webcomponents-lite
 //= require_tree ./tutor
 //= require ./admin/contents_form/media_uploader_helper.js
