7e293b3f853b7fa7c540b0429592cd465bce9814
Darwin Gualotuña <daosgc@gmail.com>
2019-12-26 16:26:40 -0500

    Update shared mail and update styles

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,18 +10,14 @@ class UserMailer < ApplicationMailer
     @user_name = name
     @public_url = public_url
     @image_url = image_url
-    @profile_url = "http://moi.growmoi.com/#/user/"+name+"/profile"
-    @url_dashboard = "http://moi-backend.growmoi.com/tutor"
-    @url_sign_in = "http://moi.growmoi.com/#/login"
-    @url_web_site = "http://growmoi.com/"
-    @url_facebook = "http://facebook.com/growmoi"
-    @url_youtube = "https://www.youtube.com/user/growmoi"
-    @url_twitter = "https://twitter.com/growmoi"
+    @profile_url = "http://aula.miaulabdp.com/#/user/"+name+"/profile"
+    @url_dashboard = "http://backend.miaulabdp.com/tutor"
+    @url_sign_in = "http://aula.miaulabdp.com"
+    @url_web_site = "http://miaulabdp.com"
+    @url_youtube = "https://www.youtube.com/user/BancoPacificoEC"
     attachments.inline['header_email_shared.png'] = File.read("#{Rails.root}/app/assets/images/header_email_shared.png")
     attachments.inline['footer_email_shared.png'] = File.read("#{Rails.root}/app/assets/images/footer_email_shared.png")
-    attachments.inline['tw_icon.png'] = File.read("#{Rails.root}/app/assets/images/tw_icon.png")
     attachments.inline['yt_icon.png'] = File.read("#{Rails.root}/app/assets/images/yt_icon.png")
-    attachments.inline['fb_icon.png'] = File.read("#{Rails.root}/app/assets/images/fb_icon.png")
     mail(to: email, subject: I18n.t("user_mailer.shared_content.subject"))
   end
 
