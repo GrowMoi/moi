ff292679742a7b61ef2fc6db2c6e742df395f2e0
Darwin Gualotuña <daosgc@gmail.com>
2019-12-26 22:55:17 -0500

    Update urls in tutor mailer

--- app/mailers/tutor_mailer.rb
+++ app/mailers/tutor_mailer.rb
@@ -1,11 +1,11 @@
 class TutorMailer < ApplicationMailer
-  default from: 'Moi Aprendizaje Social <noreply@growmoi.com>'
+  default from: 'Banco del Pacífico <noreply@pacifico.com>'
 
   def achievement_notification(tutor, client, achievement)
     @tutor_name = if tutor.name.present? then tutor.name else tutor.username end
     @client_name = if client.name.present? then client.name else client.username end
     @achievement_name = achievement.name
-    @url  = "http://www.growmoi.com"
+    @url  = "http://miaulabdp.com"
     mail(to: tutor.email, subject: I18n.t("tutor_mailer.achievement_notification.subject"))
   end
 
@@ -15,16 +15,12 @@ class TutorMailer < ApplicationMailer
     @authorization_key = authorization_key
     @url_dashboard = "http://backend.miaulabdp.com/tutor"
     @url_sign_in = "http://backend.miaulabdp.com/users/sign_in"
-    @url_web_site = "http://growmoi.com/"
-    @url_facebook = "http://facebook.com/growmoi"
-    @url_youtube = "https://www.youtube.com/user/growmoi"
-    @url_twitter = "https://twitter.com/growmoi"
+    @url_web_site = "http://miaulabdp.com"
+    @url_youtube = "https://www.youtube.com/user/BancoPacificoEC"
     attachments.inline['moi_title.png'] = File.read("#{Rails.root}/app/assets/images/moi_title.png")
     attachments.inline['moicartaimg.png'] = File.read("#{Rails.root}/app/assets/images/moicartaimg.png")
     attachments.inline['footer_email_shared.png'] = File.read("#{Rails.root}/app/assets/images/footer_email_shared.png")
-    attachments.inline['tw_icon.png'] = File.read("#{Rails.root}/app/assets/images/tw_icon.png")
     attachments.inline['yt_icon.png'] = File.read("#{Rails.root}/app/assets/images/yt_icon.png")
-    attachments.inline['fb_icon.png'] = File.read("#{Rails.root}/app/assets/images/fb_icon.png")
     attachments.inline['moi_logo.png'] = File.read("#{Rails.root}/app/assets/images/moi_logo.png")
     mail(to: email, subject: I18n.t("tutor_mailer.payment_account.subject"))
   end
@@ -34,16 +30,12 @@ class TutorMailer < ApplicationMailer
     @tutor_name = name
     @url_dashboard = "http://backend.miaulabdp.com/tutor"
     @url_sign_in = "http://backend.miaulabdp.com/users/sign_in"
-    @url_web_site = "http://growmoi.com/"
-    @url_facebook = "http://facebook.com/growmoi"
-    @url_youtube = "https://www.youtube.com/user/growmoi"
-    @url_twitter = "https://twitter.com/growmoi"
+    @url_web_site = "http://miaulabdp.com"
+    @url_youtube = "https://www.youtube.com/user/BancoPacificoEC"
     attachments.inline['moi_title.png'] = File.read("#{Rails.root}/app/assets/images/moi_title.png")
     attachments.inline['moicartaimg.png'] = File.read("#{Rails.root}/app/assets/images/moicartaimg.png")
     attachments.inline['footer_email_shared.png'] = File.read("#{Rails.root}/app/assets/images/footer_email_shared.png")
-    attachments.inline['tw_icon.png'] = File.read("#{Rails.root}/app/assets/images/tw_icon.png")
     attachments.inline['yt_icon.png'] = File.read("#{Rails.root}/app/assets/images/yt_icon.png")
-    attachments.inline['fb_icon.png'] = File.read("#{Rails.root}/app/assets/images/fb_icon.png")
     attachments.inline['moi_logo.png'] = File.read("#{Rails.root}/app/assets/images/moi_logo.png")
     mail(to: email, subject: I18n.t("tutor_mailer.payment_account.subject"))
   end
