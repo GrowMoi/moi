bd2d0511e2f0c00cfef452077aa508b47e166cdf
Mario Andrés Correa <a@macool.me>
2020-02-12 12:06:06 -0500

    query leaderboard by: users’: school, age or city
    supports multiple queries
    
    eg:
    
    http://localhost:3005/api/leaderboard.json?page=1&per_page=10&user_id=21&sort_by=city&age=12&school=APC

--- app/services/public_leaderboard_service.rb
+++ app/services/public_leaderboard_service.rb
@@ -2,12 +2,15 @@ class PublicLeaderboardService
   def initialize(params:)
     @params = params
     set_params_sorting_options!
+    set_params_query_options!
   end
 
   def all_leaders
-    Leaderboard
-      .joins(:user)
-      .order(sorting_options)
+    main_scope = Leaderboard.joins(:user).order(sorting_options)
+    if @params_query_options.present?
+      main_scope = main_scope.where(@params_query_options)
+    end
+    main_scope
   end
 
   private
@@ -21,6 +24,16 @@ class PublicLeaderboardService
       @params_sorting_options << User.arel_table[:city].asc
     when "age"
       @params_sorting_options << User.arel_table[:age].asc
+    end
+  end
+
+  def set_params_query_options!
+    @params_query_options = {}
+    user_query_params_options = [ :school, :city, :age ]
+    user_query_params_options.each do |query_opt|
+      if @params[query_opt].present?
+        @params_query_options[:"users.#{query_opt}"] = @params[query_opt]
+      end
     end
   end
 
