8820e054779837b62b45a471f0dfb1588b0884e9
Mario Andrés Correa <a@macool.me>
2020-04-01 04:21:41 -0500

    notify chats via pusher

--- app/services/user_chat_service.rb
+++ app/services/user_chat_service.rb
@@ -3,12 +3,31 @@ class UserChatService
     @sender = sender
   end
 
+  ##
+  # @return [UserChat] instance of user chat
+  #   with errors if there are any. you may
+  #   call #persisted?
   def send_message(chat_params)
-    UserChat.create(
+    user_chat = UserChat.create(
       sender_id: @sender.id,
       receiver_id: chat_params[:receiver_id],
       message: chat_params[:message]
     )
+    if user_chat.persisted?
+      notify_via_pusher!(user_chat)
+    end
+    user_chat
+  end
+
+  def notify_via_pusher!(user_chat)
+    return if Rails.env.test?
+    notification_serialized = ::Api::UserChatSerializer.new(
+      user_chat,
+      root: false,
+      scope: user_chat.receiver
+    )
+    user_channel_general = "userchatsnotifications.#{user_chat.receiver_id}"
+    Pusher.trigger(user_channel_general, 'newmessage', notification_serialized)
   end
 
   def retrieve_messages(receiver_id:)
