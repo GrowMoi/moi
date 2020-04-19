f17d5cb83281c5e7385d9b9a5a054d0c19fc0eae
Jhymer Martínez <jhmartinez1991@gmail.com>
2020-04-19 09:32:18 -0500

    Enable to validate token when send multipart/form-data

--- app/controllers/tutor/base_controller/json_requests_forgery_bypass.rb
+++ app/controllers/tutor/base_controller/json_requests_forgery_bypass.rb
@@ -5,13 +5,16 @@ module Tutor
 
       included do
         skip_before_action :verify_authenticity_token,
-                           if: :json_request?
+                           if: :valid_header?
       end
 
       protected
 
-      def json_request?
-        request.headers["Content-Type"].include?("application/json") if request.headers["Content-Type"].present?
+      def valid_header?
+        json_request = request.headers["Content-Type"].include?("application/json") if request.headers["Content-Type"].present?
+        form_data = request.headers["Content-Type"].include?("multipart/form-data") if request.headers["Content-Type"].present?
+
+        json_request || form_data
       end
     end
   end
