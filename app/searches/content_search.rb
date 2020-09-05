610becce51648b1b6f16f3112c5a5585649888c6
Lenin Capa <leviskp87@gmail.com>
2020-09-04 20:41:02 -0500

    support query includes special characters

--- app/searches/content_search.rb
+++ app/searches/content_search.rb
@@ -3,6 +3,9 @@ class ContentSearch < Searchlight::Search
   searches :q
 
   def search_q
+    unless @q.blank?
+      q = ActiveSupport::Inflector.transliterate(@q).to_s
+    end
     search.where("unaccent(contents.title) ILIKE :q OR unaccent(contents.description) ILIKE :q", q: "%#{q}%")
           .where(['approved', true])
   end
