d84c2830e8c8eb066b385602a3d214931be43d13
Lenin Capa <leviskp87@gmail.com>
2020-09-23 16:41:05 -0500

    update gem rest-client and change cloudinary credentials

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,4 @@
 # hack: set cloudinary URL for dev
 if Rails.env.development?
-  ENV["CLOUDINARY_URL"] = "cloudinary://234492286161472:55GOQ_bKx4KxioWgLpZMFYvT84Q@hprtcgwsj"
+  ENV["CLOUDINARY_URL"] = "cloudinary://686632149634842:-KbPFR0lDcjUb5BIQU8PDa1kmi4@moi-images"
 end
