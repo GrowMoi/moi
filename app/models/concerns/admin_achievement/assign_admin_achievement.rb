a887777789d75480869f0af69af7dbc62d4d4721
Lenin Capa <leviskp87@gmail.com>
2020-10-18 14:17:17 -0500

    clean code and improvements

--- app/models/concerns/admin_achievement/assign_admin_achievement.rb
+++ app/models/concerns/admin_achievement/assign_admin_achievement.rb
@@ -2,23 +2,19 @@ class AdminAchievement < ActiveRecord::Base
   module AssignAdminAchievement
 
     def user_win_achievement?(user)
-      case self.number
-      when 1
-        user_learnt_n_content(self, user)
-      when 2..5
-        learnt_contents_in_branch(self, user)
-      when 6
-        learnt_all_contents(user)
-      when 7
-        learnt_a_content_in_each_public_neuron(user)
-      when 8
-        successful_continous_tests(self, user)
-      when 9
-        tests_given(self, user)
-      when 10
-        user_reach_level(self, user)
+      case self.category
+      when "content"
+        by_content(self, user)
+      when "branch"
+        by_branch(self, user)
+      when "test"
+        by_test(self, user)
+      when "level"
+        by_level(self, user)
+      when "neuron"
+        by_neuron(self, user)
       else
-        user_learnt_content_in_a_neuron(self, user)
+        puts "no achievement found"
       end
     end
 
@@ -26,73 +22,78 @@ class AdminAchievement < ActiveRecord::Base
 
     ##
     # user learnt n contents
-    def user_learnt_n_content(achievement, user)
-      number = achievement.settings['quantity'].to_i
-      user.content_learnings.size >= number
-    end
+    def by_content(achievement, user)
+      completed = false
+      settings = achievement.settings || {};
 
-    ##
-    # user gave n tests
-    def tests_given(achievement, user)
-      number = achievement.settings['quantity'].to_i
-      user.learning_tests.size >= number
-    end
-
-    ##
-    # user gave tests without errors
-    def successful_continous_tests(achievement, user)
-      number = achievement.settings['quantity'].to_i
-      user.continuous_successful_tests(number)
-    end
-
-    ##
-    # user learnt all contest public/approved
-    def learnt_all_contents(user)
-      total_contents = Neuron.approved_public_contents.count
-      user.content_learnings.size == total_contents
-    end
-
-    ##
-    # user learnt almost a content by public neuron
-    def learnt_a_content_in_each_public_neuron(user)
-      public_neurons = Neuron.where(is_public: true, active: true)
-                             .sort_by(&:position)
-      give_achievement = false
-      runLoop = true
-      i = 0
-      until runLoop == false || i == public_neurons.count
-        neuron = public_neurons[i]
-        runLoop = user.already_learnt_any?(neuron.contents)
-        give_achievement = runLoop
-        i += 1
+      if settings["branch"] == "any" && settings["quantity"].to_i > 0
+        number = settings['quantity'].to_i
+        completed = user.content_learnings.size >= number
       end
-      give_achievement
+
+      if settings["branch"] == "all" && settings["quantity"].to_i > 0
+        public_neurons = Neuron.where(is_public: true, active: true)
+                                .sort_by(&:position)
+        give_achievement = false
+        runLoop = true
+        i = 0
+        until runLoop == false || i == public_neurons.count
+          neuron = public_neurons[i]
+          runLoop = user.already_learnt_any?(neuron.contents)
+          give_achievement = runLoop
+          i += 1
+        end
+        completed = give_achievement
+      end
+
+      #all contents learnt
+      if settings["branch"] == "all" && settings["quantity"].to_i == 0
+        total_contents = Neuron.approved_public_contents.count
+        completed = user.content_learnings.size == total_contents
+      end
+      completed
+    end
+
+    ##
+    # user approved n tests
+    def by_test(achievement, user)
+      completed = false
+      settings = achievement.settings || {};
+      if settings["continuous"]
+        number = achievement.settings['quantity'].to_i
+        completed = user.continuous_successful_tests(number)
+      else
+        number = achievement.settings['quantity'].to_i
+        completed = user.learning_tests.size >= number
+      end
+      completed
     end
 
     ##
     # user learnt content specific branch
-    def learnt_contents_in_branch(achievement, user)
+    def by_branch(achievement, user)
+      completed = false
       name = achievement.settings['branch']
       contents = user.contents_learnt_by_branch(name)
       unless contents.blank?
-        contents.size >= achievement.settings['quantity'].to_i
-      else
-        false
+        completed = contents.size >= achievement.settings['quantity'].to_i
       end
+      completed
     end
 
     ##
     # user reach a specific level
-    def user_reach_level(achievement, user)
+    def by_level(achievement, user)
       user_tree = TreeService::UserTreeFetcher.new(user, nil)
-      user_tree.depth == achievement.settings['level']
+      user_tree.depth.to_i == achievement.settings['level'].to_i
     end
 
     ##
     # user learnt content in a specific neuron
-    def user_learnt_content_in_a_neuron(achievement, user)
-      quantity = achievement.settings['quantity'].to_i
-      neuron_id = achievement.settings['neuron_id'].to_i
+    def by_neuron(achievement, user)
+      settings = achievement.settings['quantity'] || {};
+      quantity = settings['quantity'].to_i
+      neuron_id = settings['neuron_id'].to_i
       total_learnt_contents = user.content_learnings.where(neuron_id: neuron_id).count
       total_learnt_contents >= quantity
     end
